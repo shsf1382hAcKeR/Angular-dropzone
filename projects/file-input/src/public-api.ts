8bbd34a503eefbf7658d01b5a8cdf0d1b958346f
Siavash Samadi <shsf1382@gmail.com>
2023-09-17 13:37:45 +0330

    Fix `public-api.ts`

--- projects/file-input/src/public-api.ts
+++ projects/file-input/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of file-input
  */
 
-export * from './lib/services/angular-dropzone.service';
+export * from './lib/services/FileProcess.service';
 export * from './lib/components/angular-dropzone.component';
 export * from './lib/angular-dropzone.module';
